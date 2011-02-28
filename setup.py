32d48a43ad2dd80d2e5479905c511aca42290993
nferrier <none@none>
2011-02-28 02:13:20 +0000

    new version for new page stuff

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ classifiers = [
 # django
 setup(
     name = "django-xslt",
-    version = "0.4",
+    version = "0.4.1",
     description = "an XSLT template system for Django",
     long_description = """A replacment for Django's template system based on XSLT.""",
     license = "BSD",
