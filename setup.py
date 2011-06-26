b3c42ed5730b64adf7a37fd228de0e662f2268df
nferrier <none@none>
2011-06-27 00:18:04 +0100

    new version 0.4.5 - fixes some merge problems?

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ classifiers = [
 # django
 setup(
     name = "django-xslt",
-    version = "0.4.4",
+    version = "0.4.5",
     description = "an XSLT template system for Django",
     long_description = """A replacment for Django's template system based on XSLT.""",
     license = "BSD",
